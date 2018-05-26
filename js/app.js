5035341c0ad6d8dd402ca4e515ada2cd23be0122
Juan <juan@johnson.com>
2018-05-26 18:58:14 -0300

    Added markers, trying to add animation to them.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,53 @@
 var map;
 
-var markers = [
+var places = [
       {
-        coords:{lat: -34.9056020, lng: -56.1825029},
-        content:'marker1'
+        coords:{lat: -34.9056020, lng: -56.1825029},       
       },
       {
         coords:{lat: -34.9031735, lng: -56.1808506},
-        content:'marker2'
       },
       {
-        coords:{lat: -34.9048805, lng: -56.1896483},
-        content:'marker3'      
+        coords:{lat: -34.9048805, lng: -56.1896483},    
       }
     ];
 
 function ViewModel() {
+
+    var self = this;
+
+    this.markers = [];
     
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.905603, lng: -56.186499},
         zoom: 16
     });
    
-       
+   
+    this.InfoWindow = new google.maps.InfoWindow();
 
-    markers.forEach(function(marker){
-          new google.maps.Marker({
-          position:marker.coords,
-          map : map,
+    places.forEach(function(place){
+      this.markerCoords = place.coords;
+
+      this.marker = new google.maps.Marker({
+        map: map,
+        position: place.coords,
+        animation: google.maps.Animation.DROP
       });
+      this.marker.addListener('click', self.markerAnimation);
+      self.markers.push(this.marker); 
+      
+      
     });
 
-    
-
-
-
-
-    
+    this.markerAnimation = function() {
+      this.setAnimation(google.maps.Animation.BOUNCE);
+    };
+        
+         
+          
     /*var infowindow = new google.maps.InfoWindow({
-        content: "Do you ever feel, like a plastic bag"
+        content: "Do you ever feel like a plastic bag"
     });
     
     marker.addListener('click', function(){
