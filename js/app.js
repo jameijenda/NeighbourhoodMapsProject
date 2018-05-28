bac1b187aa4eaab255b42360bc330a0a76a5cfee
Juan <juan@johnson.com>
2018-05-28 01:02:33 -0300

    Infowindows test work!

--- js/app.js
+++ js/app.js
@@ -2,12 +2,15 @@ var map;
 
 var places = [
       {
+        title: 'marker2',
         coords:{lat: -34.9056020, lng: -56.1825029},       
       },
       {
+        title: 'marker3',
         coords:{lat: -34.9031735, lng: -56.1808506},
       },
       {
+        title: 'marker1',
         coords:{lat: -34.9048805, lng: -56.1896483},    
       }
     ];
@@ -24,7 +27,7 @@ function ViewModel() {
     });
    
    
-    this.InfoWindow = new google.maps.InfoWindow();
+    
 
     places.forEach(function(place){
       this.markerCoords = place.coords;
@@ -34,15 +37,27 @@ function ViewModel() {
         position: place.coords,
         animation: google.maps.Animation.DROP
       });
+
+      var contentString = place.title;
+
+      this.InfoWindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+      self.markers.push(this.marker); 
+
+
       this.marker.addListener('click', function(){
         this.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout((function() {
             this.setAnimation(null);
         }).bind(this), 1400);
-      });  
-      self.markers.push(this.marker); 
-      
-      
+
+        InfoWindow.setContent(contentString);
+        InfoWindow.open(map, this);
+
+      });              
+    
     });
                           
 };
