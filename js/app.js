2b94a986744925887e5b25816b8c6192e9b071be
Juan <juan@johnson.com>
2018-06-05 00:45:33 -0300

    Got infowindows to pull info from Foursquare API.

--- js/app.js
+++ js/app.js
@@ -2,22 +2,29 @@ var map;
 
 var places = [
       {
-        title: 'marker2',
-        coords:{lat: -34.9056020, lng: -56.1825029},       
+        title: 'Automovil Club del Uruguay',
+        coords:{lat: -34.9035340070997, lng: -56.1890231974923
+        },       
       },
       {
-        title: 'marker3',
-        coords:{lat: -34.9031735, lng: -56.1808506},
+        title: 'Grupocine Torre de los Profesionales',
+        coords:{lat: -34.90273716428635, lng: -56.1880319023078
+        },
       },
       {
-        title: 'marker1',
-        coords:{lat: -34.9048805, lng: -56.1896483},    
-      }
+        title: 'El Gaucho',
+        coords:{lat: -34.90524373610124, lng: -56.18509797993519},    
+        },
+      {
+        title: 'Subway',
+        coords:{lat: -34.90608759593457, lng: -56.19157126828292},    
+        }
     ];
 
 function ViewModel() {
 
     var self = this;
+
 
     this.markers = [];
     
@@ -25,12 +32,10 @@ function ViewModel() {
         center: {lat: -34.905603, lng: -56.186499},
         zoom: 16
     });
-   
-   
 
+    
 
     places.forEach(function(place){
-      /*this.markerCoords = place.coords;*/
 
       this.marker = new google.maps.Marker({
         map: map,
@@ -38,14 +43,15 @@ function ViewModel() {
         animation: google.maps.Animation.DROP
       });
 
-      var contentString = place.title;
+      
 
-      var lat = this.marker.position.lat;
-      var lng = this.marker.position.lng;
 
-      this.InfoWindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+      var lat = place.coords.lat;
+      var lng = place.coords.lng;
+      var title = place.title;
+
+
+      this.InfoWindow = new google.maps.InfoWindow();
 
       self.markers.push(this.marker); 
 
@@ -57,23 +63,36 @@ function ViewModel() {
         }).bind(this), 1200);
         
         
-        //I'm getting a param error while using the lat and lng variables defined above
-
       
-        fetch('https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&v=20180323')
+        fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&query=${title}&v=20180323`)
+        
         .then(function(response){
-          return response.json();
-        });
-     
+          var json = response.json();
+          return json;
+        })
+        
+        .then(function(myJson){
+          self.businessName = myJson.response.venues[0].name;
+          self.street = myJson.response.venues[0].location.address;
+          self.category = myJson.response.venues[0].categories[0].name;
+          console.log(self.businessName, self.street, self.category);
 
-        InfoWindow.setContent(contentString);
-        InfoWindow.open(map, this);
+          self.infoWindowContent = `<h2 class="businessName"> ${self.businessName}</h2>                   
+                                    <h3 class="category"> ${self.category}</h3>
+                                    <p4 class="address"> ${self.street}</p4>`;
+          });
+        
+         
 
-      });              
+          InfoWindow.setContent(self.infoWindowContent);
+          InfoWindow.open(map, this);
+
+      }); 
+
     
     });
 
-
+   
                           
 };
 
