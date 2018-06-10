c233e359c8ad1dee38e9d6524516a4b92a545d08
Juan <juan@johnson.com>
2018-06-10 20:21:09 -0300

    Working on fixing clicking items on the list.

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ function ViewModel() {
 
     this.markers = [];
     this.filteredResults = [];
+    this.test;
     
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.905603, lng: -56.186499},
@@ -63,47 +64,47 @@ function ViewModel() {
       
 
       
-      marker.addListener('click', function(){
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout((function() {
-            this.setAnimation(null);
-        }).bind(this), 1200);
-
-        fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&query=${title}&v=20180323`)
-        
-        .then(function(response){
-          var json = response.json();
-          return json;
-        })
-        
-        .then(function test(myJson){
-          self.businessName = myJson.response.venues[0].name;
-          self.street = myJson.response.venues[0].location.address;
-          self.category = myJson.response.venues[0].categories[0].name;
-
-          /* This console.log is a test to see if the right values come through */
-
-          console.log(self.businessName, self.street, self.category);
-
-          self.infoWindowContent = `<h2 class="businessName"> ${self.businessName}</h2>                   
-                                    <h3 class="category"> ${self.category}</h3>
-                                    <p4 class="address"> ${self.street}</p4>`;
-          })
-
-          .catch(function(){
-            alert(`
-                  Oops! The Foursquare API could not be loaded.
-                  Please reload your page and hope for the best`)
-          })
-        
-          
+      self.test = marker.addListener('click', function(){
+          this.setAnimation(google.maps.Animation.BOUNCE);
           setTimeout((function() {
-            self.InfoWindow.setContent(self.infoWindowContent);      
-            self.InfoWindow.open(map, this);;
-          }).bind(this), 500);
+              this.setAnimation(null);
+          }).bind(this), 1200);
 
-                              
-      }); 
+          fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&query=${title}&v=20180323`)
+          
+          .then(function(response){
+            var json = response.json();
+            return json;
+          })
+          
+          .then(function test(myJson){
+            self.businessName = myJson.response.venues[0].name;
+            self.street = myJson.response.venues[0].location.address;
+            self.category = myJson.response.venues[0].categories[0].name;
+
+            /* This console.log is a test to see if the right values come through */
+
+            console.log(self.businessName, self.street, self.category);
+
+            self.infoWindowContent = `<h2 class="businessName"> ${self.businessName}</h2>                   
+                                      <h3 class="category"> ${self.category}</h3>
+                                      <p4 class="address"> ${self.street}</p4>`;
+            })
+
+            .catch(function(){
+              alert(`
+                    Oops! The Foursquare API could not be loaded.
+                    Please reload your page and hope for the best`)
+            })
+          
+            
+            setTimeout((function() {
+              self.InfoWindow.setContent(self.infoWindowContent);      
+              self.InfoWindow.open(map, this);;
+            }).bind(this), 500);
+
+                                
+        }); 
 
     
     });
@@ -124,36 +125,8 @@ function ViewModel() {
       return self.filteredResults;
     }, this);
 
-
-    /*
-
-    Search function:
-
-    Create Ko.Observable upstairs.
-
-    Here, create ko.computed function.
-
-    Create empty array.
-
-    forEach loop. {
-    marker location = current marker.
-
-    if (markerLocation(or current marker) .includes this.searchOption() 
-
-    push the resut to markerLocation;
-    and make marker visible) 
-    else {
-      make marker invisible
-    }
-
-    return result.
-
-    yayyyyy
-
-    }*/
-
    
-                          
+                         
 };
 
 
