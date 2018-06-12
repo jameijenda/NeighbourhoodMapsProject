b5a06d2fe41ab5979db19d388f7ed53d0ef29397
Juan <juan@johnson.com>
2018-06-12 03:27:29 -0300

    Previous commit was not compatible with KO list, had to rewrite it. Clicking list items works now!

--- js/app.js
+++ js/app.js
@@ -28,86 +28,111 @@ function ViewModel() {
 
     this.markers = [];
     this.filteredResults = [];
-    this.test;
+
     
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.905603, lng: -56.186499},
-        zoom: 16
-    });
+    this.createMapAndMarkers = function() { 
 
-    this.InfoWindow = new google.maps.InfoWindow({
-      content: "Placeholder"
-    });
-    
-
-    places.forEach(function(place){
-
-      this.marker = new google.maps.Marker({
-        map: map,
-        position: place.coords,
-        title: place.title,
-        animation: google.maps.Animation.DROP
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.905603, lng: -56.186499},
+          zoom: 16
       });
 
-      
-
-
-      var lat = place.coords.lat;
-      var lng = place.coords.lng;
-      var title = place.title;
+      this.InfoWindow = new google.maps.InfoWindow({
+        content: "Placeholder"
+      });
 
 
       
 
-      self.markers.push(this.marker); 
+      places.forEach(function(place){
 
-      
+        this.marker = new google.maps.Marker({
+          map: map,
+          position: {
+            lat: place.coords.lat,
+            lng: place.coords.lng
+          },
+          title: place.title,
+          animation: google.maps.Animation.DROP
+        });
 
-      
-      self.test = marker.addListener('click', function(){
-          this.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout((function() {
-              this.setAnimation(null);
-          }).bind(this), 1200);
 
-          fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&query=${title}&v=20180323`)
+
+        self.markers.push(this.marker); 
+
+        this.marker.addListener('click', self.animationAndInfo);
+
+      });
+
+    };
+
           
-          .then(function(response){
-            var json = response.json();
-            return json;
-          })
-          
-          .then(function test(myJson){
-            self.businessName = myJson.response.venues[0].name;
-            self.street = myJson.response.venues[0].location.address;
-            self.category = myJson.response.venues[0].categories[0].name;
+      
 
-            /* This console.log is a test to see if the right values come through */
+    this.animationAndInfo = function() {
+      self.getAPIDataAndPopulate(this);
+      this.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout((function() {
+          this.setAnimation(null);
+      }).bind(this), 1200);
 
-            console.log(self.businessName, self.street, self.category);
+      setTimeout((function() {
+          self.InfoWindow.setContent(self.infoWindowContent);      
+          self.InfoWindow.open(map, this);;
+        }).bind(this), 500);
+    };      
 
-            self.infoWindowContent = `<h2 class="businessName"> ${self.businessName}</h2>                   
-                                      <h3 class="category"> ${self.category}</h3>
-                                      <p4 class="address"> ${self.street}</p4>`;
-            })
+      
 
-            .catch(function(){
-              alert(`
-                    Oops! The Foursquare API could not be loaded.
-                    Please reload your page and hope for the best`)
-            })
-          
-            
-            setTimeout((function() {
-              self.InfoWindow.setContent(self.infoWindowContent);      
-              self.InfoWindow.open(map, this);;
-            }).bind(this), 500);
 
-                                
-        }); 
+
+
+    this.getAPIDataAndPopulate = function(data) {
+
+
+          var lat = data.getPosition().lat();
+          var lng = data.getPosition().lng();
+          var title = data.title;
+
+          // Foursquare API call
+
+      fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&client_id=SVZ5OCK2BVJSNEOMS3PRUVZGIBD52HTF0YO4WJSNHNXYMHZ1&client_secret=GNW4DFY2KRSIVOKQHRI221XBF3QNF2RMRFDZROKOYRSDZ4AI&query=${title}&v=20180323`)
+      
+      .then(function(response){
+        var json = response.json();
+        return json;
+      })
+      
+      .then(function test(myJson){
+        self.businessName = myJson.response.venues[0].name;
+        self.street = myJson.response.venues[0].location.address;
+        self.category = myJson.response.venues[0].categories[0].name;
+
+        /* This console.log is a test to see if the right values come through */
+
+        console.log(self.businessName, self.street, self.category);
+
+        self.infoWindowContent = `<h2 class="businessName"> ${self.businessName}</h2>                   
+                                  <h3 class="category"> ${self.category}</h3>
+                                  <p4 class="address"> ${self.street}</p4>`;
+        })
+
+        .catch(function(){
+          alert(`
+                Oops! The Foursquare API could not be loaded.
+                Please reload your page and hope for the best`)
+        });
+
+
+    };
+
+    this.createMapAndMarkers();
 
     
-    });
+    
+    
+    
+    
 
     this.searchInput = ko.observable('');
 
@@ -125,9 +150,10 @@ function ViewModel() {
       return self.filteredResults;
     }, this);
 
-   
-                         
 };
+
+  
+                       
 
 
 function render() {
